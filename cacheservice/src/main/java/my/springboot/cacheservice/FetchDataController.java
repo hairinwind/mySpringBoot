26872559af47dbb957892c0bf7fba14da1d0fab1
Yao Dong <ydong@luxoft.com>
2019-07-26 09:52:11 -0700

    remove unused code

--- cacheservice/src/main/java/my/springboot/cacheservice/FetchDataController.java
+++ cacheservice/src/main/java/my/springboot/cacheservice/FetchDataController.java
@@ -24,11 +24,6 @@ public class FetchDataController {
         return "cache 'data' is cleared...";
     }
 
-    @GetMapping("/clearCache")
-    public String clearCacheProgramatically(){
-        Cachemanager
-    }
-
     @GetMapping("/fetchNonCacheData")
     public int fetchNonCacheData(@RequestParam(value="name", defaultValue="test") String name) {
         return fetchDataService.fetchNonCacheData(name);
