aee7f82e7141ff0d750fc8142b4f5dde79f09278
Yao Dong <ydong@luxoft.com>
2019-07-27 21:12:54 -0700

    swagger

--- swagger/src/main/java/my/springboot/swagger/SwaggerConfig.java
+++ swagger/src/main/java/my/springboot/swagger/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage( "my.springboot.swagger" )) //this gets rid of "basic-error-controller" from springfox swagger-ui
                 .paths(PathSelectors.any())
                 .build();
     }
