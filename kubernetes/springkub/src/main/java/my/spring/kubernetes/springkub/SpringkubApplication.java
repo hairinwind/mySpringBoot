9901504288c1f992ba45133fda04a2b7f21de807
yao <dong_yao@hotmail.com>
2021-03-25 21:27:09 -0700

    initial springkub

---
+++ kubernetes/springkub/src/main/java/my/spring/kubernetes/springkub/SpringkubApplication.java
@@ -0,0 +1,25 @@
+package my.spring.kubernetes.springkub;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringkubApplication {
+
+	@Value("${config.msg}")
+	public String message;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkubApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return message;
+	}
+
+}
---
+++ kubernetes/springkub/src/test/java/my/spring/kubernetes/springkub/SpringkubApplicationTests.java
@@ -0,0 +1,13 @@
+package my.spring.kubernetes.springkub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
