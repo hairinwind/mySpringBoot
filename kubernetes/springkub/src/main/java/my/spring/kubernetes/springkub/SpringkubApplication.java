437875ed1bafc1600bf39b52d09d4e0b9b57cfa6
yao <dong_yao@hotmail.com>
2021-03-31 14:15:50 -0700

    springkub the config server client

--- kubernetes/springkub/src/main/java/my/spring/kubernetes/springkub/SpringkubApplication.java
+++ kubernetes/springkub/src/main/java/my/spring/kubernetes/springkub/SpringkubApplication.java
@@ -3,11 +3,13 @@ package my.spring.kubernetes.springkub;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope //this has to be added wherever @Value is used, if you want it to be refreshed after /actuator/refresh
 public class SpringkubApplication {
 
 	@Value("${config.msg}")
