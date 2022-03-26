f7c704068ae6c129503bb69ad3ea9f874830e9cc
hairinwind <dong_yao@hotmail.com>
2022-03-26 16:08:59 -0700

    my spring security

--- security/src/main/java/my/spring/security/SecuredContorller.java
+++ security/src/main/java/my/spring/security/SecuredContorller.java
@@ -1,4 +1,20 @@
 package my.spring.security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SecuredContorller {
+
+    @GetMapping("/protected")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_DEVELOPERS')")
+    public String protectedEndpoint() {
+        return "this is protected";
+    }
+
+    @GetMapping("/notprotected")
+    public String notProtectedEndpoint() {
+        return "this is NOT protected";
+    }
 }
