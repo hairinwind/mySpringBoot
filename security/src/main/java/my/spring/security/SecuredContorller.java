4b40b292c3b644cd72efe42858b3405afb121a07
hairinwind <dong_yao@hotmail.com>
2022-03-27 17:09:44 -0700

    spring security

--- security/src/main/java/my/spring/security/SecuredContorller.java
+++ security/src/main/java/my/spring/security/SecuredContorller.java
@@ -1,16 +1,27 @@
 package my.spring.security;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Collection;
 
 @RestController
 public class SecuredContorller {
 
     @GetMapping("/protected")
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_DEVELOPERS')")
-    public String protectedEndpoint() {
-        return "this is protected";
+    public String protectedEndpoint(Authentication authentication) { //spring injects Authentication
+        // Here is the sample code to programatically get authorities from authentication
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for(GrantedAuthority au : authorities) {
+            System.out.println(authentication.getName() + " has role " + au.getAuthority());
+        }
+
+        return "this is protected for " + authentication.getName();
     }
 
     @GetMapping("/notprotected")
