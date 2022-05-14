8bcc627a7fd2c4731a0b845ae69a71d5dd07880a
hairinwind <dong_yao@hotmail.com>
2022-05-14 14:13:27 -0700

    check in

---
+++ my-argument-resolver/src/main/java/my/spring/myargumentresolver/MyArgumentResolverApplication.java
@@ -0,0 +1,13 @@
+package my.spring.myargumentresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyArgumentResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyArgumentResolverApplication.class, args);
+	}
+
+}
